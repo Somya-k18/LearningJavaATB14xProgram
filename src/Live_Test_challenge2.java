3ed2f71ee86c7aaf6072df83dc8f7baeebedae4f
Somya-k18 <somyak1812@gmail.com>
2025-10-12 15:00:52 +0530

    Live Test Challenge 2

---
+++ src/Live_Test_challenge2.java
@@ -0,0 +1,29 @@
+public class Live_Test_challenge2 {
+    public static void main(String[] args) {
+        /*Create a Java program that demonstrates various arithmetic and assignment operators.
+
+        **Requirements:**
+        - Use +, -, *, /, % operators
+        - Demonstrate assignment operators (=, +=, -=, *=, /=)
+        - Show the results of each operation*/
+        //Input: 10 5
+
+        //Output:
+        //Addition: 15 Subtraction: 5 Multiplication: 50 Division: 2 Modulus: 0
+
+        int num1 = 10;
+        int num2 = 5;
+        int result = num1 + num2;
+        int result1 = num1 - num2;
+        int result2  = num1 * num2;
+        int result3 = num1 / num2;
+        int result4 = num1 % num2;
+        System.out.println("Addition : "+ result);
+        System.out.println("Substraction : "+ result1);
+        System.out.println("Multiplication : "+ result2);
+        System.out.println("Division : "+ result3);
+        System.out.println("Modulus : "+ result4);
+
+
+    }
+}
